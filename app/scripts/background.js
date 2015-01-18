6f9388621d99f3abaf61a9c88a46065ef5a982af
aliaksandr-pasynkau <alxe.master@gmail.com>
2015-01-18 17:17:44 +0300

    clean scripts

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,7 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+  //console.log('previousVersion', details.previousVersion);
 });
 
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+//chrome.browserAction.setBadgeText({text: '30'});
